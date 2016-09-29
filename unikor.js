5ce78c0c883ce50afc3af367a255d13f57a02bc7
Gyusun Yeom <omniavinco@gmail.com>
2016-09-29 23:34:01 +0900

    드보락 추가 외 잡다한 수정 (#2)
    * composite medial
    
    * Dvorak
    
    * add contributor
    
    * re-convert when type is changed

--- unikor.js
+++ unikor.js
@@ -43,10 +43,21 @@ UniKor = {
                 }
                 initial = newInit;
             } else if (ch >= 0x314f && ch <= 0x3163) {
+                var newMedial = ch - 0x314f;
                 if (medial !== null) {
-                    flush();
+                    if (fin === null) {
+                        if (medial === 8 && _.includes([0, 1, 20], newMedial)) {
+                            newMedial = 9 + _.indexOf([0, 1, 20], newMedial);
+                        } else if (medial === 13 && _.includes([4, 5, 20], newMedial)) {
+                            newMedial = 14 + _.indexOf([4, 5, 20], newMedial);
+                        } else if (medial === 18 && newMedial === 20) {
+                            newMedial = 19;
+                        }
+                    } else {
+                        flush();
+                    }
                 }
-                medial = ch - 0x314f;
+                medial = newMedial;
             } else if (ch >= 0x11a8 && ch <= 0x11c2) {
                 if (fin !== null) {
                     flush();
@@ -68,17 +79,31 @@ UniKor = {
                 var fi = ch % 28;
                 ch /= Math.floor(28);
                 var medial = ch % 21;
+                if (UniKor.composited.hasOwnProperty(medial)) {
+                    medial = UniKor.composited[medial];
+                } else {
+                    medial = [medial];
+                }
                 var initial = Math.floor(ch / 21);
                 var out = [];
                 if (fi !== 0) {
                     out.unshift(String.fromCharCode(fi + 0x11a7));
                 }
-                out.unshift(String.fromCharCode(medial + 0x314f));
+                _.forEach(medial, function(m) { out.unshift(String.fromCharCode(m + 0x314f)); });
                 out.unshift(["ㄱ", ["ㄱ", "ㄱ"], "ㄴ", "ㄷ", ["ㄷ", "ㄷ"], "ㄹ", "ㅁ", "ㅂ", ["ㅂ", "ㅂ"], "ㅅ", ["ㅅ", "ㅅ"], "ㅇ", "ㅈ", ["ㅈ", "ㅈ"], "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"][initial]);
                 return out;
             } else {
                 return och;
             }
         }));
+    },
+    composited:{
+        9: [8, 0],
+        10: [8, 1],
+        11: [8, 20],
+        14: [13, 4],
+        15: [13, 5],
+        16: [13, 20],
+        19: [18, 20]
     }
 }
